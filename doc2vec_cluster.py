4be5e45d73e5ef4affa31d8625d157d2fed1a4b7
exdodo <29415318+exdodo@users.noreply.github.com>
2018-10-11 10:48:32 +0900

    Update doc2vec_cluster.py

--- doc2vec_cluster.py
+++ doc2vec_cluster.py
@@ -15,17 +15,35 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from collections import defaultdict
 
+def word2vec_most_sim(model, posls):
+    # similarityチェック
+    return model.wv.most_similar(positive=posls, topn=1)
+
+def wv_list(doc,model):
+    docls=[]
+    for doc_item in doc :
+        try :
+            vector = model.wv[doc_item]
+            docls.append(doc_item)
+        except KeyError :
+            continue
+    return docls
+
 if __name__ == "__main__":
     dir_model=r'C:\\Users\\Yusuke.SERVICE\\Documents\\DATABASE\\word2vecmodel\\'
     model_name='xbrl_doc2vecmodel'
     df1 = pd.read_csv('EdinetcodeDlInfo.csv',encoding='cp932',header=1,index_col=0)
-    cluster_num=100 #分類数
+    cluster_num=10 #分類数
+    print('分類数',cluster_num)
+    print('分類するモデル',model_name)
     edinetdic=df1['提出者名'].to_dict()
     #\u3000削除
     edinetdic={x:v.replace('\u3000','') for x,v in edinetdic.items()}
     # モデルのロード
     d2v_model = gensim.models.Doc2Vec.load(dir_model+model_name)
-    #m2= gensim.models.Doc2Vec.load(dir_model+'xbrl_doc2vecmodeloverview')
+    #wiki_model = gensim.models.word2vec.Word2Vec.load(dir_model + 'wiki_model')
+    category_model=d2v_model #wiki_model
+    print('分類の名前付けに使うモデル',category_model)
     #kMeans
     kmeans_model = KMeans(n_clusters=cluster_num, init='k-means++', max_iter=100)  
     X = kmeans_model.fit(d2v_model.docvecs.vectors_docs)
@@ -40,8 +58,16 @@ if __name__ == "__main__":
     for cluster_id, doc_tag in zip(labels, doc_tags):
         cluster_to_docs[cluster_id].append(doc_tag)
     #クラスター出力
-    for docs in cluster_to_docs.values():
-        print(docs)
+        for docs in cluster_to_docs.values():
+        #各クラスターのタグ名をwikiより指定
+        #doc要素から’株式会社’削除
+        doc=[i.replace('株式会社','') for i in docs ]
+        docls=wv_list(doc,category_model) #wiki_model or d2v_model
+        if(docls==[]):
+            print('分類名','該当なし')
+        else :
+            print('分類名',word2vec_most_sim(category_model,docls)) #wiki_model or d2v_mode
+        #print(docs) #分類の個別企業を見る場合
     #分布をグラフ化    
     l = kmeans_model.fit_predict(d2v_model.docvecs.vectors_docs)
     pca = PCA(n_components=2).fit(d2v_model.docvecs.vectors_docs)
