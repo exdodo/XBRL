bc16920c4b80e7d9ca46be7800a44edee4f0c455
exdodo <29415318+exdodo@users.noreply.github.com>
2019-04-01 08:17:59 +0900

    Update EDINET_API.py

--- EDINET_API.py
+++ EDINET_API.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Mar 28 15:34:50 2019
+q(-::-) 〆(-：：-) (-：：-)P ｼﾞｪｲｿﾝ
 edinet apiを使い過去の書類一覧のjson化
 ・どのくらいの頻度のrequestsを送って良いか不明
 ・3日で1M　5年だと600M位のサイズになるのかな
@@ -22,48 +23,75 @@ from urllib3.exceptions import InsecureRequestWarning
 urllib3.disable_warnings(InsecureRequestWarning) #verify=False対策
 from itertools import chain
 from time import sleep
+import os
+import pickle
 
 def jsonToList(sdt):    
     url = 'https://disclosure.edinet-fsa.go.jp/api/v1/documents.json'
     params = {'date': sdt, 'type': 2}
     headers = {'User-Agent': 'hoge'}
     res = requests.get(url, params=params, verify=False,timeout=3.5, headers=headers)
-    rjson=res.json()    
-    result_list=rjson['results']    
-    return result_list
+    sleep(1) #1秒間をあける
+    rjson=res.json()
+    return rjson
+def datelog():
+    #読込日を覚えておく
+    if os.path.exists('datelog.pkl') :
+        with open('datelog.pkl','rb') as log_file:
+            datelogs=pickle.load(log_file)
+    else : datelogs=[]
+    datelogs.sort()
+    if(len(datelogs)>(1826)) : #過去5年を超える日付は削除
+        del datelogs[1825:]                
+    return datelogs
 
-def json_docs() :    
-    dt_today = date.today()
-    dt_5y = dt_today-timedelta(days=3) #約5年前なら5*365　正確さを求めるならrelativedelta
+def json_docs(dt_5y,dt_today,df_doc1) :    
     doc_list=[]
-    while(dt_5y <= dt_today):
-        sdt=dt_5y.strftime('%Y-%m-%d')
-        jsons=jsonToList(std)
-        if  jsons!=[] :
-            doc_list.append(jsons)
-        sleep(1)
+    datelogs=datelog()
+    while(dt_5y < dt_today):
+        sdt=dt_5y.strftime('%Y-%m-%d')        
+        if sdt not in datelogs : #過去に読み込んだ日付は読み込まない
+            datelogs.append(sdt)
+            rjson=jsonToList(sdt)
+            jsons=rjson['results']              
+            if  jsons!=[] : doc_list.append(jsons)            
         dt_5y=dt_5y+timedelta(days=1)
-    docs=list(chain.from_iterable(doc_list))  #flatten  
+        
+    docs=list(chain.from_iterable(doc_list))  #flatten    
     df_docs=pd.io.json.json_normalize(docs) #To Dataframe
-    #df_docs.to_excel('xbrldocs.xls',encoding='cp932')
-    #df_docs.to_hdf('xbrldocs.h5',df_docs) 未完成
-    df_docs.to_json('xbrldocs.json')
+    df_concat1=pd.concat([df_doc1, df_docs])
+    df_concat1.to_json('xbrldocs.json')
+    with open('datelog.pkl','wb') as log_file:
+        pickle.dump(datelogs, log_file)
+    print(datelogs)
+    
+def get_xbrl(docID) :
+    #書類取得
+    url = 'https://disclosure.edinet-fsa.go.jp/api/v1/documents/'+docID
+    params = { 'type': 2}
+    headers = {'User-Agent': 'hoge'}
+    res = requests.get(url, params=params,verify=False,timeout=3.5, headers=headers)
+    sleep(1) #1秒間をあける
+    contentType = res.headers['Content-Type']
+    contentDisposition = res.headers['Content-Disposition']
+    ATTRIBUTE = 'filename='
+    fileName = contentDisposition[contentDisposition.find(ATTRIBUTE) + len(ATTRIBUTE):]
+    print(contentType,fileName)
     
 if __name__=='__main__':
+    last_day=date.today()
+    start_day=last_day-timedelta(days=2) #約5年前なら5*365　正確さを求めるならrelativedelta
+    if last_day < start_day : start_day=last_day    
+    if os.path.exists('xbrlmetas.json') :  df_doc1=pd.read_json('xbrldocs.json')
+    else : df_doc1 = pd.DataFrame(index=[])
     #過去ｎ日文の書類リストをまとめる
-    json_docs()    
-    
+    json_docs(start_day,last_day,df_doc1)    
+'''    
     #docIDを取得する
     df_read = pd.read_json('xbrldocs.json')
     docIDs=df_read['docID'].to_list()
     
     #書類取得
-    url = 'https://disclosure.edinet-fsa.go.jp/api/v1/documents/'+docIDs[0]
-    params = { 'type': 1}
-    headers = {'User-Agent': 'hoge'}
-    res = requests.get(url, params=params,verify=False,timeout=3.5, headers=headers)
-    contentType = res.headers['Content-Type']
-    contentDisposition = res.headers['Content-Disposition']
-    ATTRIBUTE = 'filename='
-    fileName = contentDisposition[contentDisposition.find(ATTRIBUTE) + len(ATTRIBUTE):]
-    print(fileName)
+    get_xbrl(docIDs[0])
+'''    
+    
