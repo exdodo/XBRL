3e10cb39963f6f1789f78ffbb0b07eeb32e48985
exdodo <29415318+exdodo@users.noreply.github.com>
2019-04-01 15:50:19 +0900

    Update EDINET_API.py

--- EDINET_API.py
+++ EDINET_API.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ -*- coding: utf-8 -*-
 """
 Created on Thu Mar 28 15:34:50 2019
 q(-::-) 〆(-：：-) (-：：-)P ｼﾞｪｲｿﾝ
@@ -25,6 +25,7 @@ from itertools import chain
 from time import sleep
 import os
 import pickle
+from tqdm import tqdm
 
 def jsonToList(sdt):    
     url = 'https://disclosure.edinet-fsa.go.jp/api/v1/documents.json'
@@ -36,39 +37,43 @@ def jsonToList(sdt):
     return rjson
 def datelog():
     #読込日を覚えておく
+    datelogs=[] 
     if os.path.exists('datelog.pkl') :
-        with open('datelog.pkl','rb') as log_file:
-            datelogs=pickle.load(log_file)
-    else : datelogs=[]
-    datelogs.sort()
+        with open('datelog.pkl','rb') as log:
+            datelogs=pickle.load(log)            
+            datelogs.sort()        
     if(len(datelogs)>(1826)) : #過去5年を超える日付は削除
         del datelogs[1825:]                
     return datelogs
 
 def json_docs(dt_5y,dt_today,df_doc1) :    
     doc_list=[]
-    datelogs=datelog()
+    datelogs=datelog()    
+    pbar = tqdm(total=(dt_today-dt_5y).days)
     while(dt_5y < dt_today):
         sdt=dt_5y.strftime('%Y-%m-%d')        
         if sdt not in datelogs : #過去に読み込んだ日付は読み込まない
             datelogs.append(sdt)
             rjson=jsonToList(sdt)
-            jsons=rjson['results']              
-            if  jsons!=[] : doc_list.append(jsons)            
+            if rjson['metadata']['status']=='200':
+                if  rjson['results'] !=[] : 
+                    doc_list.append(rjson['results'])                
+        pbar.update(1)
         dt_5y=dt_5y+timedelta(days=1)
-        
+    pbar.close()    
+    with open('datelog.pkl','wb') as log_file:
+        datelogs.sort()
+        pickle.dump(datelogs, log_file)    
     docs=list(chain.from_iterable(doc_list))  #flatten    
     df_docs=pd.io.json.json_normalize(docs) #To Dataframe
     df_concat1=pd.concat([df_doc1, df_docs])
     df_concat1.to_json('xbrldocs.json')
-    with open('datelog.pkl','wb') as log_file:
-        pickle.dump(datelogs, log_file)
-    print(datelogs)
+    
     
 def get_xbrl(docID) :
     #書類取得
     url = 'https://disclosure.edinet-fsa.go.jp/api/v1/documents/'+docID
-    params = { 'type': 1}
+    params = { 'type': 2}
     headers = {'User-Agent': 'hoge'}
     res = requests.get(url, params=params,verify=False,timeout=3.5, headers=headers)
     sleep(1) #1秒間をあける
@@ -80,7 +85,7 @@ def get_xbrl(docID) :
     
 if __name__=='__main__':
     last_day=date.today()
-    start_day=last_day-timedelta(days=2) #約5年前なら5*365　正確さを求めるならrelativedelta
+    start_day=last_day-timedelta(days=365) #約5年前なら5*365　正確さを求めるならrelativedelta
     if last_day < start_day : start_day=last_day    
     if os.path.exists('xbrlmetas.json') :  df_doc1=pd.read_json('xbrldocs.json')
     else : df_doc1 = pd.DataFrame(index=[])
