41e663d8933545d9ee0e871a86e6899858617ed6
exdodo <29415318+exdodo@users.noreply.github.com>
2019-04-02 10:28:27 +0900

    Update EDINET_API.py

--- EDINET_API.py
+++ EDINET_API.py
@@ -1,11 +1,11 @@
- -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 Created on Thu Mar 28 15:34:50 2019
 q(-::-) 〆(-：：-) (-：：-)P ｼﾞｪｲｿﾝ
 edinet apiを使い過去の書類一覧のjson化
 ・どのくらいの頻度のrequestsを送って良いか不明
-・3日で1M　5年だと600M位のサイズになるのかな
-と言うわけで実行はしていない
+・1年分試してみたら約50M 10分
+
 書類一覧が日付指定でしか取れないのは改善して欲しいな
 参考URL
 http://d.hatena.ne.jp/xef/20121027/p2
@@ -27,7 +27,7 @@ import os
 import pickle
 from tqdm import tqdm
 
-def jsonToList(sdt):    
+def request_json(sdt):    
     url = 'https://disclosure.edinet-fsa.go.jp/api/v1/documents.json'
     params = {'date': sdt, 'type': 2}
     headers = {'User-Agent': 'hoge'}
@@ -46,29 +46,30 @@ def datelog():
         del datelogs[0:len(datelogs)-1826]                
     return datelogs
 
-def json_docs(dt_5y,dt_today,df_doc1) :    
+def json_docs(dt_5y,dt_today) :    
     doc_list=[]
-    datelogs=datelog()    
+    datelogs=datelog()  #過去の読込日を呼び出す  
     pbar = tqdm(total=(dt_today-dt_5y).days)
     while(dt_5y < dt_today):
         sdt=dt_5y.strftime('%Y-%m-%d')        
         if sdt not in datelogs : #過去に読み込んだ日付は読み込まない
             datelogs.append(sdt)
-            rjson=jsonToList(sdt)
+            rjson=request_json(sdt)
             if rjson['metadata']['status']=='200':
-                if  rjson['results'] !=[] : 
-                    doc_list.append(rjson['results'])                
+                rjson_list=rjson['results']
+                if  rjson_list !=[] :
+                    doc_list.append(rjson_list)                
         pbar.update(1)
         dt_5y=dt_5y+timedelta(days=1)
-    pbar.close()    
+    
+    pbar.close()      
+    docs=list(chain.from_iterable(doc_list))  #flatten    
+    df_doc2=pd.io.json.json_normalize(docs) #To Dataframe
+    df_doc2=df_doc2.reset_index(drop=True)
     with open('datelog.pkl','wb') as log_file:
         datelogs.sort()
-        pickle.dump(datelogs, log_file)    
-    docs=list(chain.from_iterable(doc_list))  #flatten    
-    df_docs=pd.io.json.json_normalize(docs) #To Dataframe
-    df_concat1=pd.concat([df_doc1, df_docs])
-    df_concat1.to_json('xbrldocs.json')
-    
+        pickle.dump(datelogs, log_file)
+    return df_doc2
     
 def get_xbrl(docID) :
     #書類取得
@@ -85,12 +86,18 @@ def get_xbrl(docID) :
     
 if __name__=='__main__':
     last_day=date.today()
-    start_day=last_day-timedelta(days=365) #約5年前なら5*365　正確さを求めるならrelativedelta
+    start_day=last_day-timedelta(days=8) #約5年前なら5*365　正確さを求めるならrelativedelta
     if last_day < start_day : start_day=last_day    
-    if os.path.exists('xbrlmetas.json') :  df_doc1=pd.read_json('xbrldocs.json')
-    else : df_doc1 = pd.DataFrame(index=[])
-    #過去ｎ日文の書類リストをまとめる
-    json_docs(start_day,last_day,df_doc1)    
+    df_doc2=json_docs(start_day,last_day) #過去ｎ日文の書類リストをまとめる
+    
+    #concat
+    if os.path.exists('xbrldocs.json') :  
+        df_doc1=pd.read_json('xbrldocs.json')
+        df_concat=pd.concat([df_doc1,df_doc2],sort=True)
+        df_concat=df_concat.reset_index(drop=True)
+        df_concat.to_json('xbrldocs.json')
+    else : df_doc2.to_json('xbrldocs.json')
+        
 '''    
     #docIDを取得する
     df_read = pd.read_json('xbrldocs.json')
@@ -99,4 +106,3 @@ if __name__=='__main__':
     #書類取得
     get_xbrl(docIDs[0])
 '''    
-    
